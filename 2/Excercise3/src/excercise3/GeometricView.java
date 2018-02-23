4087648e3c9189c0439904650dc0a8100210009d
Unknown <n.korporaal@student.ru.nl>
2018-02-23 13:41:44 +0100

    queryInput() update
    Added a list of possible commands

--- 2/Excercise3/src/excercise3/GeometricView.java
+++ 2/Excercise3/src/excercise3/GeometricView.java
@@ -20,6 +20,7 @@ public class GeometricView {
     public static String[] queryInput(){
         Scanner reader = new Scanner(System.in);
         System.out.println("Enter a command, followed by parameters");
+        System.out.println("Possible commands are: \nquit, show, circle x y r, rectangle x y h w, move i dx dy, remove i, sort, sort x, sort y");
         String s = reader.nextLine();
         return s.split(" ");
     }
